17bdb5d7abaad3014687cfd304248aad906afb2a
gaurav <kotharigaurav6@gmail.com>
2024-08-10 19:04:11 +0530

    array

---
+++ 7 Array/ArrayDemo1.java
@@ -0,0 +1,13 @@
+// program showing the concept of array
+
+class ArrayDemo1{
+    public static void main(String args[]){
+        //int arr[]={1,2,3};
+        int arr[]=new int[]{1,2,3};
+
+        System.out.println("arr[0] : "+arr[0]);
+        System.out.println("arr[1] : "+arr[1]);
+        System.out.println("arr[2] : "+arr[2]);
+    }
+}
+
---
+++ 7 Array/ArrayDemo2.java
@@ -0,0 +1,25 @@
+// program showing the concept of array
+
+import java.util.Scanner;
+class ArrayDemo2{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size : ");
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter array elements : ");
+        for(int i=0;i<size;i++){
+           System.out.println("Enter "+i+" indexing element : "); 
+           arr[i] = sc.nextInt();     
+        }
+
+        System.out.println("\nArray elements are : \n");
+        for(int i=0;i<size;i++){
+           System.out.print("\t"+arr[i]);     
+        }
+
+    }
+}
+
